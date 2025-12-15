46e17a6fe50254f9b57c5234e870944f20ff22e5
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-15 20:05:44 +0500

    App.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react';
-import { AuthProvider } from '../auth/useAuth';
+import { SessionProvider } from '../auth/useSession';
 import { TournamentProvider } from '../tournament/useTournament';
 import Router from './router';
+import PinLogin from '../components/PinLogin';
 
 // Root component that sets up all contexts
 const App = () => {
   return (
-    <AuthProvider>
+    <SessionProvider>
       <TournamentProvider>
         <Router />
+        <PinLogin /> {/* Global Modal Layer */}
       </TournamentProvider>
-    </AuthProvider>
+    </SessionProvider>
   );
 };
 

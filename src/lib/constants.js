7ac97dbd5910a862b57a282f086a579ef360e185
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-15 22:13:52 +0500

    constants.js

--- src/lib/constants.js
+++ src/lib/constants.js
@@ -1,11 +1,13 @@
 export const MAP_POOL = [
-  { id: 'ancient', name: 'Ancient', image: 'https://placehold.co/600x400/1a1a1a/ffffff?text=Ancient' },
-  { id: 'anubis', name: 'Anubis', image: 'https://placehold.co/600x400/2a2a2a/ffffff?text=Anubis' },
-  { id: 'inferno', name: 'Inferno', image: 'https://placehold.co/600x400/3a3a3a/ffffff?text=Inferno' },
-  { id: 'mirage', name: 'Mirage', image: 'https://placehold.co/600x400/4a4a4a/ffffff?text=Mirage' },
-  { id: 'nuke', name: 'Nuke', image: 'https://placehold.co/600x400/5a5a5a/ffffff?text=Nuke' },
-  { id: 'overpass', name: 'Overpass', image: 'https://placehold.co/600x400/6a6a6a/ffffff?text=Overpass' },
-  { id: 'vertigo', name: 'Vertigo', image: 'https://placehold.co/600x400/7a7a7a/ffffff?text=Vertigo' }
+  { id: 'de_ancient', name: 'Ancient', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_ancient.png' },
+  { id: 'de_anubis', name: 'Anubis', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_anubis.png' },
+  { id: 'de_dust2', name: 'Dust 2', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_dust2.png' },
+  { id: 'de_inferno', name: 'Inferno', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_inferno.png' },
+  { id: 'de_mirage', name: 'Mirage', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_mirage.png' },
+  { id: 'de_nuke', name: 'Nuke', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_nuke.png' },
+  { id: 'de_overpass', name: 'Overpass', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_overpass.png' },
+  { id: 'de_train', name: 'Train', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_train.png' },
+  { id: 'de_vertigo', name: 'Vertigo', image: 'https://raw.githubusercontent.com/sivert-io/cs2-server-manager/master/map_thumbnails/de_vertigo.png' }
 ];
 
 export const RANKS = [

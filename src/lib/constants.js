50bb3def667f5a3d5ba97fcbaa83c4e90d5ed64d
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-15 17:56:11 +0500

    constants.js

---
+++ src/lib/constants.js
@@ -0,0 +1,17 @@
+export const MAP_POOL = [
+  { id: 'ancient', name: 'Ancient', image: 'https://placehold.co/600x400/1a1a1a/ffffff?text=Ancient' },
+  { id: 'anubis', name: 'Anubis', image: 'https://placehold.co/600x400/2a2a2a/ffffff?text=Anubis' },
+  { id: 'inferno', name: 'Inferno', image: 'https://placehold.co/600x400/3a3a3a/ffffff?text=Inferno' },
+  { id: 'mirage', name: 'Mirage', image: 'https://placehold.co/600x400/4a4a4a/ffffff?text=Mirage' },
+  { id: 'nuke', name: 'Nuke', image: 'https://placehold.co/600x400/5a5a5a/ffffff?text=Nuke' },
+  { id: 'overpass', name: 'Overpass', image: 'https://placehold.co/600x400/6a6a6a/ffffff?text=Overpass' },
+  { id: 'vertigo', name: 'Vertigo', image: 'https://placehold.co/600x400/7a7a7a/ffffff?text=Vertigo' }
+];
+
+export const RANKS = [
+  'Silver I', 'Silver II', 'Silver III', 'Silver IV', 'Silver Elite', 'Silver Elite Master',
+  'Gold Nova I', 'Gold Nova II', 'Gold Nova III', 'Gold Nova Master',
+  'Master Guardian I', 'Master Guardian II', 'Master Guardian Elite',
+  'Distinguished Master Guardian', 'Legendary Eagle', 'Legendary Eagle Master',
+  'Supreme Master First Class', 'Global Elite'
+];

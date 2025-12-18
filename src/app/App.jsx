35f092a6171ad04f0d6947bdfb641911f56d7d95
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-19 03:32:59 +0500

    Appv2.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,30 +3,34 @@ import { SessionProvider } from '../auth/useSession';
 import { TournamentProvider } from '../tournament/useTournament';
 import Router from './router';
 import PinLogin from '../components/PinLogin';
-
-// Optional: Import a global error boundary here if desired
+import ErrorBoundary from '../components/ErrorBoundary';
 
 const App = () => {
   return (
-    // 1. Auth Layer (Session Management)
-    <SessionProvider>
+    // 1. Global Safety Net
+    <ErrorBoundary>
       
-      {/* 2. Data Layer (Tournament State) */}
-      <TournamentProvider>
+      {/* 2. Auth Layer */}
+      <SessionProvider>
         
-        <div className="antialiased text-zinc-300 min-h-screen bg-ui-bg selection:bg-brand selection:text-black">
+        {/* 3. Data Layer */}
+        <TournamentProvider>
           
-          {/* 3. Navigation & Views */}
-          <Router />
+          <div className="antialiased text-zinc-300 min-h-screen bg-ui-bg selection:bg-brand selection:text-black">
+            
+            {/* 4. Interface */}
+            <Router />
+            
+            {/* 5. Global Overlays */}
+            <PinLogin />
+            
+          </div>
           
-          {/* 4. Global Overlays */}
-          <PinLogin />
-          
-        </div>
+        </TournamentProvider>
         
-      </TournamentProvider>
+      </SessionProvider>
       
-    </SessionProvider>
+    </ErrorBoundary>
   );
 };
 

7beb5a8c18558fe57c0513f771322ac1bb769f7c
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-20 17:37:11 +0500

    Appv3.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,35 +1,22 @@
 import React from 'react';
+import { Outlet, useNavigate } from 'react-router-dom'; // Import Outlet
 import { SessionProvider } from '../auth/useSession';
 import { TournamentProvider } from '../tournament/useTournament';
-import Router from './router';
-import PinLogin from '../components/PinLogin';
 import ErrorBoundary from '../components/ErrorBoundary';
 
 const App = () => {
   return (
-    // 1. Global Safety Net
     <ErrorBoundary>
-      
-      {/* 2. Auth Layer */}
       <SessionProvider>
-        
-        {/* 3. Data Layer */}
         <TournamentProvider>
-          
-          <div className="antialiased text-zinc-300 min-h-screen bg-ui-bg selection:bg-brand selection:text-black">
+          <div className="antialiased text-zinc-300 min-h-screen bg-[#060709] selection:bg-[#ff5500] selection:text-black">
             
-            {/* 4. Interface */}
-            <Router />
-            
-            {/* 5. Global Overlays */}
-            <PinLogin />
-            
+            {/* Renders the child route (Bracket, Admin, or Veto) */}
+            <Outlet /> 
+
           </div>
-          
         </TournamentProvider>
-        
       </SessionProvider>
-      
     </ErrorBoundary>
   );
 };

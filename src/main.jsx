11c8a789673308e42a5622c5a11251301dda5846
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-20 18:24:07 +0500

    mainv3a.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from '../app/router';
-import './index.css';
+import { router } from './app/router'; // <--- CHANGED from '../' to './'
+import './ui/index.css'; // Adjust this if your CSS is in 'src/ui/' or just './index.css' if in 'src/'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

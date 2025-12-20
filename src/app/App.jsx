2883076fffdcfb71245c659491494f197f6dbeb3
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-20 18:18:24 +0500

    Appv3a.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Outlet, useNavigate } from 'react-router-dom'; // Import Outlet
+import { Outlet } from 'react-router-dom';
 import { SessionProvider } from '../auth/useSession';
 import { TournamentProvider } from '../tournament/useTournament';
 import ErrorBoundary from '../components/ErrorBoundary';
@@ -10,10 +10,7 @@ const App = () => {
       <SessionProvider>
         <TournamentProvider>
           <div className="antialiased text-zinc-300 min-h-screen bg-[#060709] selection:bg-[#ff5500] selection:text-black">
-            
-            {/* Renders the child route (Bracket, Admin, or Veto) */}
             <Outlet /> 
-
           </div>
         </TournamentProvider>
       </SessionProvider>

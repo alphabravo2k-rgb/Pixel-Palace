8bceb4971bd84297f2322dcc38b4e378731ceb70
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-21 00:08:01 +0500

    mainv3a.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
-import { router } from './app/router'; // Correct: looks in src/app/router.jsx
-import './index.css'; // Correct: looks in src/index.css
+import { router } from './app/router'; 
+import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>

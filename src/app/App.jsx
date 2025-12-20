007d136b7841b6116e958a37e7dbd1e3775c0938
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-21 00:07:43 +0500

    Appv3a.jsx

--- src/app/App.jsx
+++ src/app/App.jsx
@@ -3,14 +3,19 @@ import { Outlet } from 'react-router-dom';
 import { SessionProvider } from '../auth/useSession';
 import { TournamentProvider } from '../tournament/useTournament';
 import ErrorBoundary from '../components/ErrorBoundary';
+// We need AdminToolbar here so it persists across all pages
+import AdminToolbar from '../components/AdminToolbar';
+import PinLogin from '../components/PinLogin';
 
 const App = () => {
   return (
     <ErrorBoundary>
       <SessionProvider>
         <TournamentProvider>
-          <div className="antialiased text-zinc-300 min-h-screen bg-[#060709] selection:bg-[#ff5500] selection:text-black">
+          <div className="antialiased text-zinc-300 min-h-screen bg-[#060709] selection:bg-[#ff5500] selection:text-black flex flex-col">
+            <AdminToolbar />
             <Outlet /> 
+            <PinLogin /> {/* Global Modal */}
           </div>
         </TournamentProvider>
       </SessionProvider>

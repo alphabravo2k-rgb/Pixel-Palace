6c07997514e7e61734559de51f25a5b7fe41fc64
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-20 18:18:03 +0500

    routerv3a.jsx

--- src/app/router.jsx
+++ src/app/router.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
-import App from './App'; // This will become our Layout Wrapper
-import BracketView from '../components/BracketView'; // The file you just renamed
+import App from './App';
+import BracketView from '../components/BracketView'; 
 import VetoPanel from '../components/VetoPanel';
 import AdminDashboard from '../components/AdminDashboard';
 import PinLogin from '../components/PinLogin';
@@ -8,14 +9,14 @@ import PinLogin from '../components/PinLogin';
 export const router = createBrowserRouter([
   {
     path: "/",
-    element: <App />, // Wraps everything in Session/Tournament Providers
+    element: <App />,
     children: [
       {
         index: true, 
         element: (
           <>
             <BracketView />
-            <PinLogin /> {/* Overlay Login on the main bracket */}
+            <PinLogin />
           </>
         ) 
       },

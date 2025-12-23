3fa502974da2e5222293481b3028c9dbe229671f
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-23 16:02:48 +0500

    permissionsv5a.actions.js

---
+++ src/lib/permissions.actions.js
@@ -0,0 +1,10 @@
+// ✅ SINGLE SOURCE OF TRUTH FOR PERMISSION ACTIONS
+export const PERM_ACTIONS = {
+  MATCH_UPDATE: 'match.update',
+  MATCH_FORCE_WIN: 'match.force_win',
+  VETO_OVERRIDE: 'veto.override',
+  VIEW_SENSITIVE: 'view.sensitive',
+  VETO_ACT: 'veto.act',
+  VIEW_SERVER_IP: 'server.ip.view',
+  DISPUTE_RAISE: 'dispute.raise'
+};

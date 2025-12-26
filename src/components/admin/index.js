555749fda36cd85465655b2ab4b3d3b316e74aec
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-27 02:35:50 +0500

    indexv9.js

---
+++ src/components/admin/index.js
@@ -0,0 +1,9 @@
+// File: src/components/admin/index.js
+export { AdminDashboard } from './AdminDashboard';
+export { AdminToolbar } from './AdminToolbar';
+export { AdminMatchControls } from './AdminMatchControls';
+export { AdminRosterReview } from './AdminRosterReview';
+export { StaffManagement } from './StaffManagement';
+export { TeamStatusControl } from './TeamStatusControl';
+export { MatchFormatControl } from './MatchFormatControl';
+export { RosterIntegrityControl } from './RosterIntegrityControl';

1fa967924db7d1c6ac85a78a207214ab00da26c6
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-28 19:28:26 +0500

    index.v10js

--- src/components/admin/index.js
+++ src/components/admin/index.js
@@ -1,6 +1,6 @@
 export { AdminDashboard } from './AdminDashboard';
 export { AdminToolbar } from './AdminToolbar';
-export { AdminMatchControls } from './AdminMatchControls'; // ✅ Creating this fixed the build
+export { AdminMatchControls } from './AdminMatchControls';
 export { AdminMatchModal } from './AdminMatchModal';
 export { AdminRosterReview } from './AdminRosterReview';
 export { StaffManagement } from './StaffManagement';
@@ -8,3 +8,4 @@ export { TeamStatusControl } from './TeamStatusControl';
 export { MatchFormatControl } from './MatchFormatControl';
 export { RosterIntegrityControl } from './RosterIntegrityControl';
 export { AdminLogin } from './AdminLogin';
+export { BracketSwapper } from './BracketSwapper';

94ea25eafb6fc9f66c438645a977a9c276a02ef2
alphabravo2k-rgb <alphabravo2k@gmail.com>
2025-12-31 22:50:40 +0500

    clientv14.js

--- src/supabase/client.js
+++ src/supabase/client.js
@@ -19,9 +19,9 @@ export const supabase = createClient(supabaseUrl, supabaseKey, {
   },
   realtime: {
     params: {
-      eventsPerSecond: 10, // Control the frequency of realtime events
+      eventsPerSecond: 10, // Throttle updates
     },
   },
 });
 
-export const isSupabaseConfigured = supabaseUrl && supabaseKey ? true : false;
+export const isSupabaseConfigured = !!(supabaseUrl && supabaseKey);

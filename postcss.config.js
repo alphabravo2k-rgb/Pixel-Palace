66b149a6fa3e01e82e9a22e94f3a47b4ab4bb5de
alphabravo2k-rgb <alphabravo2k@gmail.com>
2026-01-05 00:27:13 +0500

    v18 postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,1 +1,10 @@
-export default { plugins: { tailwindcss: {}, autoprefixer: {}, }, }
+export default {
+  plugins: {
+    // 1. Tailwind: Compiles your utility classes
+    tailwindcss: {},
+    
+    // 2. Autoprefixer: Adds browser compatibility flags (e.g. -webkit-flex)
+    // This MUST run after Tailwind to catch the generated CSS.
+    autoprefixer: {},
+  },
+}
